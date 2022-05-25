Irina-64/taskmanager-demo

7.1.12 Реализует добавление задачи
- Создадим презентер формы добавления
- Опишем в нём логику появления и скрытия. Она будет похожа на ту, что используется в презентере задачи
- В презентере доски опишем метод createTask, который будет инициализировать презентер формы добавления
- Тут же, в презентере доски, добавим вызов метода destroy презентера формы добавления в случаях обновления доски или открытии формы редактирования
- В main.js добавим функции для обработки клика по кнопке и разблокировки кнопки после добавления задачи

src/presenter/task-new-presenter.js
@@ -1,0 +1,67 @@
+import TaskEditView from '../view/task-edit-view.js';
+import {nanoid} from 'nanoid';
+import {remove, render, RenderPosition} from '../utils/render.js';
+import {UserAction, UpdateType} from '../const.js';
+
+export default class TaskNewPresenter {
+  #taskListContainer = null;
+  #changeData = null;
+  #taskEditComponent = null;
+  #destroyCallback = null;
+
+  constructor(taskListContainer, changeData) {
+    this.#taskListContainer = taskListContainer;
+    this.#changeData = changeData;
+  }
+
+  init = (callback) => {
+    this.#destroyCallback = callback;
+
+    if (this.#taskEditComponent !== null) {
+      return;
+    }
+
+    this.#taskEditComponent = new TaskEditView();
+    this.#taskEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+    this.#taskEditComponent.setDeleteClickHandler(this.#handleDeleteClick);
+
+    render(this.#taskEditComponent, this.#taskListContainer, RenderPosition.AFTERBEGIN);
+
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  destroy = () => {
+    if (this.#taskEditComponent === null) {
+      return;
+    }
+
+    this.#destroyCallback?.();
+
+    remove(this.#taskEditComponent);
+    this.#taskEditComponent = null;
+
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  }
+
+  #handleFormSubmit = (task) => {
+    this.#changeData(
+      UserAction.ADD_TASK,
+      UpdateType.MINOR,
+      // Пока у нас нет сервера, который бы после сохранения
+      // выдывал честный id задачи, нам нужно позаботиться об этом самим
+      {id: nanoid(), ...task},
+    );
+    this.destroy();
+  }
+
+  #handleDeleteClick = () => {
+    this.destroy();
+  }
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.destroy();
+    }
+  }
+}

Shapovaloff/taskmanager-17

src/presenter/task-new-presenter.js
@@ -1,0 +1,67 @@
+import {remove, render, RenderPosition} from '../framework/render.js';
+import TaskEditView from '../view/task-edit-view.js';
+import {nanoid} from 'nanoid';
+import {UserAction, UpdateType} from '../const.js';
+
+export default class TaskNewPresenter {
+  #taskListContainer = null;
+  #changeData = null;
+  #taskEditComponent = null;
+  #destroyCallback = null;
+
+  constructor(taskListContainer, changeData) {
+    this.#taskListContainer = taskListContainer;
+    this.#changeData = changeData;
+  }
+
+  init = (callback) => {
+    this.#destroyCallback = callback;
+
+    if (this.#taskEditComponent !== null) {
+      return;
+    }
+
+    this.#taskEditComponent = new TaskEditView();
+    this.#taskEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+    this.#taskEditComponent.setDeleteClickHandler(this.#handleDeleteClick);
+
+    render(this.#taskEditComponent, this.#taskListContainer, RenderPosition.AFTERBEGIN);
+
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  destroy = () => {
+    if (this.#taskEditComponent === null) {
+      return;
+    }
+
+    this.#destroyCallback?.();
+
+    remove(this.#taskEditComponent);
+    this.#taskEditComponent = null;
+
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #handleFormSubmit = (task) => {
+    this.#changeData(
+      UserAction.ADD_TASK,
+      UpdateType.MINOR,
+      // Пока у нас нет сервера, который бы после сохранения
+      // выдывал честный id задачи, нам нужно позаботиться об этом самим
+      {id: nanoid(), ...task},
+    );
+    this.destroy();
+  };
+
+  #handleDeleteClick = () => {
+    this.destroy();
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.destroy();
+    }
+  };
+}